b1227f87470cc8bc8b9dbdf403da1b7426ed3f8e
mitsurugiRAGE <ragefist1992@gmail.com>
2025-11-12 20:35:51 +0900

    add hashing function (djb2)

--- login-sys/src/mock-login-sys.c
+++ login-sys/src/mock-login-sys.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "../include/credential.h"
+#include "../include/hash.h"
 
 int main(void)
 {
@@ -13,12 +15,14 @@ int main(void)
 
     char id_input[32]; // 사용자가 입력한 id를 저장할 버퍼
     char passwd_input[32]; // 사용자가 입력한 패스워드를 저장할 버퍼
+    char * passwd_hashed;
 
     login_window(id_input, passwd_input);
     import_credential(fp, id_buf, passwd_buf, id_input);
+    passwd_hashed = djb2((const unsigned char *)passwd_input);
   
     /* 만약 입력된 id와 패스워드가 `.cred`에 있는 것과 모두 일치한다면 */
-    if (!strcmp(id_buf, id_input) && !strcmp(passwd_buf, passwd_input))
+    if (!strcmp(id_buf, id_input) && !strcmp(passwd_buf, passwd_hashed))
     {
         // 환영 메시지 출력
         printf("WELCOME! %s!", id_buf);
@@ -31,6 +35,7 @@ int main(void)
         printf("ID or password is INCORRECT.\n");
     }
 
+    free(passwd_hashed); // `hash.c`에서 동적할당된 메모리 해제
     fclose(fp);
 
     return 0;
